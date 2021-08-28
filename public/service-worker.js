551c7b404a1e665908c599109279665e3a1b86b1
John Lanni <Johnlanni619@gmail.com>
2021-08-28 08:44:40 -0400

    finished service-worker functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = 'budget-tracker';
+const DATA_CACHE_NAME = 'budget-data-cache-v1';
 
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/css.styles.css",
+    "/css/styles.css",
     "/js/idb.js",
-    "/js/index.js"
+    "/js/index.js",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png",
 ];
 
 // Install the service worker
@@ -41,40 +49,40 @@ self.addEventListener('activate', function(evt) {
 
 // Intercept fetch requests
 self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.includes('/api/')) {
+    if (evt.request.url.includes('/api')) {
         evt.respondWith(
-          caches
-            .open(DATA_CACHE_NAME)
-            .then(cache => {
-              return fetch(evt.request)
-                .then(response => {
-                  // If the response was good, clone it and store it in the cache.
-                  if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                  }
-    
-                  return response;
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                        .then(response => {
+                            // if the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(evt.request);
+                        });
                 })
-                .catch(err => {
-                  // Network request failed, try to get it from the cache.
-                  return cache.match(evt.request);
-                });
-            })
-            .catch(err => console.log(err))
+                .catch(err => console.log(err))
         );
-        evt.respondWith(
-            fetch(evt.request).catch(function() {
-              return caches.match(evt.request).then(function(response) {
-                if (response) {
-                  return response;
-                } else if (evt.request.headers.get('accept').includes('text/html')) {
-                  // return the cached home page for all requests for html pages
-                  return caches.match('/');
-                }
-              });
-            })
-          );
-    
         return;
     }
+
+    evt.respondWith(
+        fetch(evt.request).catch(function() {
+            return caches.match(evt.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
+                }
+            });
+        })
+    );
 });
