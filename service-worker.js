5d15c1d4eea56a33b937248f83bd4f8b7b6c9afd
John Lanni <Johnlanni619@gmail.com>
2021-08-27 21:38:27 -0400

    working on service-worker functionality

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+// const APP_PREFIX = 'FoodFest-';     
+// const VERSION = 'version_01';// const CACHE_NAME = APP_PREFIX + VERSION;
+
+// const FILES_TO_CACHE = [
+//     "./server.js",
+//     "./models/transaction.js",
+//     "./public/css/styles.css",
+//     "./public/js/idb.js",
+//     "./public/js/index.js",
+//     "./public/index.html",
+//     "./routes/api.js"
+// ];
+
+// self.addEventListener('install', function (e) {
+
+// })
+
+console.log('Hi from your service-worker.js file!!');
